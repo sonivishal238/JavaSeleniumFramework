e7e0eaf09abda15f309fbf0ecd30d74657ca67c3
Vishal Soni <vishal.soni@intimetec.com>
2024-01-08 09:33:06 +0530

    continuation of the selenium course

--- SeleniumConcepts/src/intro/DriverIntro.java
+++ SeleniumConcepts/src/intro/DriverIntro.java
@@ -1,7 +1,10 @@
 package intro;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 
 public class DriverIntro {
 
@@ -15,10 +18,28 @@ public class DriverIntro {
 		// 2. Selenium Manager (introduced in Selenium 4)
 		// WebDriver driver = new ChromeDriver();
 		// WebDriver driver = new FirefoxDriver();
-		WebDriver driver = new EdgeDriver();
+		WebDriver driver = new ChromeDriver();
 
-		driver.get("https://facebook.com/");
-		System.out.println(driver.getTitle());
+//		driver.get("https://facebook.com/");
+//		System.out.println(driver.getTitle());
+		
+		driver.get("https://www.rahulshettyacademy.com/angularpractice");
+		driver.findElement(By.cssSelector("label+input[name=name]")).sendKeys("name");
+		driver.findElement(By.cssSelector("label+input[name=email]")).sendKeys("email@mail.com");
+		driver.findElement(By.cssSelector("input[type=password]")).sendKeys("password");
+		driver.findElement(By.id("exampleCheck1")).click();
+		
+		Select genderSelectionDropdown = new Select(driver.findElement(By.id("exampleFormControlSelect1")));
+		genderSelectionDropdown.selectByVisibleText("Male");
+		
+		driver.findElement(By.id("inlineRadio1")).click();
+		driver.findElement(By.cssSelector("input[type='date']")).sendKeys("05-01-2024");
+		driver.findElement(By.cssSelector("input[type='submit']")).click();
+		String text = driver.findElement(By.cssSelector("div.alert-success")).getText();
+		
+		Assert.assertTrue(text.contains("The Form has been submitted successfully!"));
+		System.out.println("Test passed");
+		
 		driver.close();
 		driver.quit();
 	}
